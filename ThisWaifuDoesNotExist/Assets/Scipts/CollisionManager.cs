060eb9d7625e0a1ce1abd40cc71453fcd4c678c4
Eren Inanci <erencan_inanci@hotmail.com>
2021-04-30 02:45:14 +0300

    Water collision detection added. Need to find better solution.

---
+++ ThisWaifuDoesNotExist/Assets/Scipts/CollisionManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    [SerializeField] private Transform woodStackedPosition;
+    [SerializeField] private Transform placedWoodPosition;
+    private int _stackedWood = 0;
+    private float _yOffset = 0;
+    private List<GameObject> _woodList = new List<GameObject>();
+    private int _index = 1;
+    public bool isOver = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wood"))
+        {
+            _yOffset += 1.25f;
+            _stackedWood++;
+            Debug.Log("Wood : " + _stackedWood);
+            other.transform.position = woodStackedPosition.transform.position;
+            other.transform.parent = woodStackedPosition.transform;
+            other.transform.rotation = woodStackedPosition.transform.rotation;
+            LeanTween.scale(other.gameObject, new Vector3(0.6375f,0.9563f, 0.6375f), 0.0f);
+            LeanTween.moveLocalY(other.gameObject, _yOffset, 0.0f);
+            _woodList.Add(other.transform.gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            Debug.Log("Player is on water.");
+            if (_woodList.Count > 0)
+            {
+                GameObject placedWood = _woodList[_woodList.Count - _index];
+                placedWood.GetComponent<BoxCollider>().isTrigger = false;
+                LeanTween.scale(placedWood, new Vector3(1, 0.06f, 0.7f), 0.0f);
+                LeanTween.move(placedWood.transform.gameObject, placedWoodPosition.transform, 0.0f);
+                placedWood.transform.rotation = placedWoodPosition.transform.rotation;
+                placedWood.transform.SetParent(null);
+                _index++;
+                _yOffset -= 1.25f;
+            }
+            else
+            {
+                isOver = true;
+                _woodList.RemoveAll(null);
+            }
+        }
+    }
+}
--- ThisWaifuDoesNotExist/Assets/Scipts/PlayerController.cs
+++ ThisWaifuDoesNotExist/Assets/Scipts/PlayerController.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 
+// ReSharper disable once CheckNamespace
 public class PlayerController : MonoBehaviour
 {
     private Touch _touch;
     private Vector2 _touchStartPos, _touchEndPos;
     [SerializeField] private float playerSpeed = 5.0f;
     [SerializeField] private float rotateSpeed = 5.0f;
+    private float _xDistance, _yDistance;
     
     void Update()
     {
         Move();
+        // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
         TouchInput();
     }
 
@@ -30,35 +33,18 @@ public class PlayerController : MonoBehaviour
             else if (_touch.phase == TouchPhase.Moved || _touch.phase == TouchPhase.Ended)
             {
                 _touchEndPos = _touch.position;
-                float x = _touchEndPos.x - _touchStartPos.x;
-                float y = _touchEndPos.y - _touchStartPos.y;
+                _xDistance = _touchEndPos.x - _touchStartPos.x;
+                _yDistance = _touchEndPos.y - _touchStartPos.y;
 
-                if (Mathf.Abs(x) == 0 && Mathf.Abs(y) == 0)
+                if (Mathf.Abs(_xDistance) > Mathf.Abs(_yDistance))
                 {
-                    Debug.Log("Tapped!");
-                }
-                else if (Mathf.Abs(x) > Mathf.Abs(y))
-                {
-                    if (x > 0)
+                    if (_xDistance > 0)
                     {
-                        Debug.Log("Right!");
                         transform.Rotate(0, 1 * rotateSpeed * Time.deltaTime, 0);
                     }
                     else
                     {
-                        Debug.Log("Left!");
                         transform.Rotate(0, -1 * rotateSpeed * Time.deltaTime, 0);
-                    }
-                }
-                else if (Mathf.Abs(x) < Mathf.Abs(y))
-                {
-                    if (y > 0)
-                    {
-                        Debug.Log("Up!");
-                    }
-                    else
-                    {
-                        Debug.Log("Down!");
                     }
                 }
             }
