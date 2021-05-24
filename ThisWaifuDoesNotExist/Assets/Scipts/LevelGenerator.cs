2a66eaab2740943bbd4762a01ab3936a6782c74d
Eren Inanci <erencan_inanci@hotmail.com>
2021-05-25 02:41:09 +0300

    5 levels.

--- ThisWaifuDoesNotExist/Assets/Scipts/LevelGenerator.cs
+++ ThisWaifuDoesNotExist/Assets/Scipts/LevelGenerator.cs
@@ -12,6 +12,10 @@ public class LevelGenerator : MonoBehaviour
         _dm = DataManager.instance;
         Debug.Log("Level started at : " + _dm.GetLevel());
         _level = _dm.GetLevel();
+        if (_level > 5)
+        {
+            _dm.level = 0;
+        }
         GenerateLevel();
     }
 
